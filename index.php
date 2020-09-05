e2f0098891f281ffcb7b2206f9f9749b9e1edd08
Soundgetcha <70407450+Soundgetcha@users.noreply.github.com>
2020-09-05 12:05:23 +0300

    Class 07

--- index.php
+++ index.php
@@ -1,103 +1,92 @@
-<html>
-	<body>
-		<h3>Действия с числами</h3>
-			<ul>
-			<li><strong>Получить остаток деления 7 на 3: </strong>
 <?php
-	$int1 = 7;
-	$int2 = 3;
-		echo ($int1 % $int2)."\n";
-		?>
-			</li>
-			<li><strong>Получить целую часть сложения 7 и 7,15: </strong>
-<?php
-	$int1 = 7;
-	$float1 = 7.15;
-		echo $int1 + (int)$float1;
-?>	
-			</li>
-			<li><strong>Получить корень из 25: </strong>
-<?php
-	$int1 = 25;
-		echo bcsqrt($int1);
-?>	
-			</li>
-			</ul>
-		
-		<h3>Действия со строками</h3>
-			<ul>
-			<li><strong>Получить 4-е слово из фразы - Десять негритят пошли купаться в море: </strong>
-<?php
-	$str = 'Десять негритят пошли купаться в море';
-	$pieces = explode(" ", $str);
-		echo $pieces[3];
-		?>
-			</li>
-			<li><strong>Получить 17-й символ из фразы - Десять негритят пошли купаться в море: </strong>
-<?php
-	$str = 'Десять негритят пошли купаться в море';
-		echo mb_strimwidth($str, 16, 1);
-?>	
-			</li>
-			<li><strong>Сделать заглавной первую букву во всех словах фразы - Десять негритят пошли купаться в море: </strong>
-<?php
-	$str = 'Десять негритят пошли купаться в море';
-	$str = mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
-		echo $str;
-?>	
-			</li>
-			<li><strong>Посчитать длину строки - Десять негритят пошли купаться в море: </strong>
-<?php
-	$str = 'Десять негритят пошли купаться в море';
-		echo mb_strlen($str);
-?>	
-			</li>
-			</ul>
-		
-		<h3>Действия с логическими значениями</h3>
-			<ul>
-			<li><strong>Правильно ли утверждение true равно 1: </strong>
-<?php
-	$boolTrue = true;
-	$int = 1;
-	if ($boolTrue == $int) {
-		echo "True"; 
+
+$study = [1, 2, 3, "dog", "apple", "true"];
+	//echo $study [3];
+	
+$study = [
+    'one' => 1,
+	'two' => 2,
+	'three' => 3,
+	'four' => 'dog',
+	'five' => 'apple',
+	'five' => 'prodigy',
+	];
+	
+	
+$study2 = [
+    1,
+	2,
+	3,
+	'three' => 3,
+	'dog',
+	'apple'
+	];	
+	
+	//var_export($study + $study2); 
+	
+	//var_export(array_values($study + $study2));
+	
+	
+$study3 = [
+    'one' => 1,
+	'two' => 2,
+	'three' => 3,
+	];	
+	
+$study4 = [
+	'two' => 2,
+	'three' => 3,
+	'one' => 1,
+	];	
+	
+$study5 = [
+
+	'two' => 2,
+	'one' => 1,
+	'three' => 3,
+	];	
+	
+	//echo $study3 ['two'];
+	//echo $study4 ['two'];
+	//echo $study5 ['two'];
+
+$arr = array_values($study3);
+$arr2 = array_values($study4);
+$arr3 = array_values($study5);
+
+ //echo $arr[1];
+ //echo $arr2[1];
+ //echo $arr3[1];
+ 
+
+	//var_export(array_keys($study));
+	
+	//var_export(array_slice($study, 2, 4));
+	
+	
+$study10 = [
+	'one' => 1,
+	'two' => [
+	    'one' => 1,
+		'two' => 2,
+		'three' => 3,
+	],	
+	'three' => 3,
+	'four'  => [
+		'one' => 1,
+		'two' => 2,
+		'three' => 3,
+	]
+	];
+	 
+	foreach($study10 as $key => $value) {
+		if (is_array($study10[$key])) {
+			foreach($study10[$key] as $key1 => $value1) {
+				echo $value1;
+			}
+		} else {
+			echo $value;
+		}
 	}
-		?>
-			</li>
-			<li><strong>Правильно ли утверждение false тождественно 0: </strong>
-<?php
-	$boolFalse = false;
-	$int = 0;
-	if ($boolFalse !== $int) {
-		echo "False"; 
-	}
-?>	
-			</li>
-						<li><strong>Какая строка длиннее three - три: </strong>
-<?php
-	$str1 = 'three';
-	$str2 = 'три';
-	if (strlen($str1) > mb_strlen($str2)) {
-		echo "$str1"; 
-	}
-?>	
-			</li>
-						<li><strong>Какое число больше 125 умножить на 13 плюс 7 или 223 плюс 28 умножить 2: </strong>
-<?php
-	$str1 = '125 умножить на 13 плюс 7';
-	$str2 = '223 плюс 28 умножить 2';
-	$int1 = 125;
-	$int2 = 13;
-	$int3 = 7;
-	$int4 = 223;
-	$int5 = 28;
-	$int6 = 2;
-	if ($int1 * $int2 + $int3 > $int4 + $int5 * $int6) {
-		echo "$str1";
-	}
-?>	
-			</li>
-			</ul>
-	</body>
-</html>
+	
+?>
